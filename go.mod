a020132c5cf8ec1f01189d476b02a2bdf6caaa1e
yulinzhihou <yulinzhihou@163.com>
2025-03-31 15:12:58 +0800

    集成 Gorilla Mux 路由

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/yulinzhihou/go-cms
 
 go 1.24.1
+
+require github.com/gorilla/mux v1.8.1 // indirect
--- main.go
+++ main.go
@@ -2,37 +2,81 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"github.com/gorilla/mux"
 )
 import "net/http"
 
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/" {
-		fmt.Fprint(w, "<h1>Hello My Go Cms</h1>\n")
-	} else {
-		w.WriteHeader(http.StatusNotFound)
-		fmt.Fprint(w, "<h1>页面未找到，请联系我们</h1>\n"+"<a href='mailto:yulinzhihou@163.com'>yulinzhihou@163.com</a>\n")
-	}
+	fmt.Fprint(w, "<h1>Hello! 欢迎来到 go-cms</h1>")
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if r.URL.Path == "/about" {
-		fmt.Fprint(w, "<h1>About</h1>\n")
-	}
+	fmt.Fprint(w, "<h1>about页</h1>")
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>not found</h1>")
+}
+
+// 文章详情
+func articlesShowHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	fmt.Fprintf(w, "<h1>文章ID ： %s</h1>", id)
+}
+
+// 文章列表页
+func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>文章列表页</h1>\n")
+}
+
+// 文章创建页
+func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>文章创建页</h1>\n")
+}
+
+// 文章更新
+func articlesUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>文章更新</h1>\n")
+}
+
+// 文章删除
+func articlesDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>文章删除</h1>\n")
+}
+
+// 文件保存
+func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, "<h1>文件保存</h1>\n")
 }
 
 func main() {
-	router := http.NewServeMux()
-	router.HandleFunc("/", defaultHandler)
-	router.HandleFunc("/about", aboutHandler)
+	//router := http.NewServeMux()
+	router := mux.NewRouter()
+	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
+	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
 
-	// 文章详情
-	router.HandleFunc("/articles/", func(writer http.ResponseWriter, request *http.Request) {
-		id := strings.SplitN(request.URL.Path, "/", 3)[2]
-		fmt.Fprint(writer, "<h1>文章详情id：</h1>\n", id)
-	})
+	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
+	router.HandleFunc("/articles", articlesStoreHandler).Methods("POST").Name("articles.store")
+
+	router.HandleFunc("/articles/create", articlesCreateHandler).Methods("GET").Name("article.create")
+	router.HandleFunc("/articles/{id:[0-9]+}", articlesShowHandler).Methods("GET").Name("articles.show")
+
+	router.NotFoundHandler = http.HandlerFunc(notFoundHandler)
+	// 通过命名路由来获取 URL 示例
+	homeUrl, _ := router.Get("home").URL()
+	fmt.Println("homeUrl = ", homeUrl)
+	articleUrl, _ := router.Get("articles.show").URL("id", "123123")
+	fmt.Println("articleUrl = ", articleUrl)
 
 	http.ListenAndServe(":8081", router)
 }
